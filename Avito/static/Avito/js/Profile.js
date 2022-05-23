60346760fe6a0eeb4484a78a171d9e78e97a180f
bifk <75476142+bifk@users.noreply.github.com>
2022-05-23 15:37:43 +0700

    Add files via upload

---
+++ Avito/static/Avito/js/Profile.js
@@ -0,0 +1,25 @@
+ function Treturn()
+{
+    window.location.href = '/';
+}
+
+function Change()
+{
+    if (document.querySelector('#change').value === "Редактировать")
+    {
+        document.querySelector('#change').value = "Сохранить"
+        document.querySelector('.inp').disabled = ""
+        document.querySelector('#inp1').disabled = ""
+    }
+    else if (document.querySelector('#change').value === "Сохранить")
+    {
+        document.querySelector('#change').value = "Редактировать"
+        document.querySelector('.inp').disabled = "disabled"
+        document.querySelector('#inp1').disabled = "disabled"
+    }
+}
+
+function CreateNote()
+{
+    window.location.href = '/add'
+}
