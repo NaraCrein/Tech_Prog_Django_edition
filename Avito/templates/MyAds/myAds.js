f5142ac98d0ef792945e8fdf27ef74c8e966fd9d
yanfomin <arthuraschee@gmail.com>
2022-05-23 23:49:42 +0700

    страница "Мои объявления" и "редактировать"

---
+++ Avito/templates/MyAds/myAds.js
@@ -0,0 +1,13 @@
+function goToSign(flag) {
+    if (flag){
+        window.location.href = '../Profile/Profile.html';
+    }
+    window.location.href = '../Sign/SignIn.html';
+}
+function goToDetail(){
+    window.location.href = '../Detail/detail.html';
+}
+
+function goToEdit(){
+    window.location.href = '../EditAd/editAd.html';
+}
