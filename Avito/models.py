8bfc60e6c46815237923f90fc6de973193c3ba2d
bifk <75476142+bifk@users.noreply.github.com>
2022-05-25 09:21:34 +0700

    Add files via upload

--- Avito/models.py
+++ Avito/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Ad(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to="images/")
-    image2 = models.ImageField(upload_to="images/")
-    image3 = models.ImageField(upload_to="images/")
-    desc = models.CharField(max_length=255)
-    seller = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    place = models.CharField(max_length=255, null=True)
+    image = models.ImageField(upload_to="images/", null=True)
+    image2 = models.ImageField(upload_to="images/", null=True)
+    image3 = models.ImageField(upload_to="images/", null=True)
+    desc = models.CharField(max_length=255, null=True)
+    seller = models.CharField(max_length=255, null=True)
 
 
